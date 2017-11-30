3246f34913bb459f8d2316744f6395814473255b
Christopher Metz <chrismetz20@gmail.com>
2017-11-30 15:15:48 -0700

    Initial Upload of Lab Assignment
    Looks like all I have is an lab assignment instead of a working version, still seems mighty helpful

---
+++ Client.java
@@ -0,0 +1,95 @@
+package start;
+
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+
+@SuppressWarnings("serial")
+public class Client extends JFrame {
+
+	private static final String ADDRESS = "localhost";
+
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		String username = JOptionPane.showInputDialog("Username?");
+		new Client(username).setVisible(true);
+	}
+
+	DefaultListModel<String> model;
+	JTextField field;
+	Socket socket;
+	String username;
+	ObjectOutputStream oos;
+	ObjectInputStream ois;
+
+	public Client(String username) throws UnknownHostException, IOException {
+		this.username = username;
+		setupModelAndLayout();
+
+		openConnection();
+
+		field.addActionListener(new FieldListener());
+		
+		// TODO 7: Start a new ServerListener thread
+	}
+
+	private void setupModelAndLayout() {
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(500, 600);
+		setLayout(new FlowLayout());
+
+		model = new DefaultListModel<String>();
+
+		JList<String> messages = new JList<>(model);
+		JScrollPane scroll = new JScrollPane(messages);
+		scroll.setPreferredSize(new Dimension(500, 500));
+		add(scroll);
+
+		field = new JTextField();
+		field.setToolTipText("Send a message here!");
+		field.setPreferredSize(new Dimension(500, 30));
+		add(field);
+	}
+
+	private void openConnection() {
+		/* Our server is on our computer, but make sure to use the same port. */
+		try {
+			// TODO 6: Connect to the Server
+
+			oos = new ObjectOutputStream(socket.getOutputStream());
+			ois = new ObjectInputStream(socket.getInputStream());
+			model.addElement("Connected to server at " + ADDRESS + ":" + Server.SERVER_PORT);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private class FieldListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO 8: When the enter button is pressed, send the contents of the
+			// JTextField to the server (add the username for extra style!)
+		};
+	}
+
+	private class ServerListener extends Thread {
+
+		@Override
+		public void run() {
+			// TODO 9: Repeatedly accept String objects from the server and add
+			// them to our model.
+		}
+	}
+}
---
+++ Server.java
@@ -0,0 +1,72 @@
+package start;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Server {
+
+	public static final int SERVER_PORT = 9001;
+
+	private static ServerSocket sock;
+	private static List<ObjectOutputStream> clients = Collections.synchronizedList(new ArrayList<>());
+
+	public static void main(String[] args) throws IOException {
+		sock = new ServerSocket(SERVER_PORT);
+		System.out.println("Server started on port " + SERVER_PORT);
+
+		while (true) {
+			// TODO 1: Accept a connection from the ServerSocket.
+			Socket s = null;
+
+			ObjectInputStream is = new ObjectInputStream(s.getInputStream());
+			ObjectOutputStream os = new ObjectOutputStream(s.getOutputStream());
+
+			// TODO 2: Save the output stream to our clients list so we can
+			// broadcast to this client later
+
+			// TODO 3: Start a new ClientHandler thread for this client.
+
+			System.out.println("Accepted a new connection from " + s.getInetAddress());
+		}
+	}
+}
+
+class ClientHandler extends Thread {
+
+	ObjectInputStream input;
+	List<ObjectOutputStream> clients;
+
+	public ClientHandler(ObjectInputStream input, List<ObjectOutputStream> clients) {
+		this.input = input;
+		this.clients = clients;
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+
+			String s = null;
+
+			// TODO 4: Read a String from the client
+
+			writeStringToClients(s);
+
+		}
+
+	}
+
+	private void writeStringToClients(String s) {
+		// TODO 5: Send a string to all clients in the client list
+	}
+
+	// TODO (When you get the chance): Write a method that closes all the
+	// resources of a ClientHandler and logs a message, and call it from every
+	// place that a fatal error occurs in ClientHandler (the catch blocks that
+	// you can't recover from).
+}
