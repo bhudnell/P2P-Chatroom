0828a5b132614567acef8bbe9abaf016545ba700
Christopher Metz <chrismetz20@gmail.com>
2017-12-02 16:44:34 -0700

    Split GUI and main

--- ChatClientGUI.java
+++ ChatClientGUI.java
@@ -15,87 +15,42 @@ import javax.swing.JTextField;
 
 public class ChatClientGUI extends JFrame {
 
-  public static void main(String[] args) {
-    new ChatClientGUI();
-  }
+	private String name;
+	private boolean firstEntry = true;
+	private JTextField outgoing;
+	private Client client;
+	public JTextArea inputFromServerTextArea;
 
-  private String name;
-  private boolean firstEntry = true;
-  private JTextField outgoing;
-  private ObjectOutputStream writer;
-  private ObjectInputStream inputFromServer;
-  private Socket socketServer;
-  public static String host = "localhost";
-  
-  private JTextArea inputFromServerTextArea;
+	public ChatClientGUI(Client client) {
+		this.client = client;
+		setTitle("Chat Client");
+		setSize(380, 480);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container cp = getContentPane();
+		cp.setLayout(null);
 
-  public ChatClientGUI() {
-    setTitle("Chat Client");
-    setSize(380, 480);
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    Container cp = getContentPane();
-    cp.setLayout(null);
+		outgoing = new JTextField("Replace me with your name");
+		outgoing.addActionListener(new InputFieldListener());
+		outgoing.setSize(300, 20);
+		outgoing.setLocation(30, 10);
+		cp.add(outgoing);
 
-    outgoing = new JTextField("Replace me with your name");
-    outgoing.addActionListener(new InputFieldListener());
-    outgoing.setSize(300, 20);
-    outgoing.setLocation(30, 10);
-    cp.add(outgoing);
+		inputFromServerTextArea = new JTextArea();
 
-    inputFromServerTextArea = new JTextArea();
+		JScrollPane scroller = new JScrollPane(inputFromServerTextArea);
+		scroller.setSize(300, 400);
+		scroller.setLocation(30, 40);
+		scroller.setBackground(Color.WHITE);
+		cp.add(scroller);
 
-    JScrollPane scroller = new JScrollPane(inputFromServerTextArea);
-    scroller.setSize(300, 400);
-    scroller.setLocation(30, 40);
-    scroller.setBackground(Color.WHITE);
-    cp.add(scroller);
+		setVisible(true);
+		outgoing.requestFocus();
+	}
 
-    setVisible(true);
-    outgoing.requestFocus();
-    makeConnectionAndReadAllServerOutputFromServer();
-  }
-
-  private void makeConnectionAndReadAllServerOutputFromServer() {
-    try {
-      // host could be "localhost", port could be 4000
-      socketServer = new Socket(host, ChatServer.PORT_NUMBER);
-      writer = new ObjectOutputStream(socketServer.getOutputStream());
-      inputFromServer = new ObjectInputStream(socketServer.getInputStream());
-      System.out.println("Found server who accepted me");
-    } catch (IOException ex) {
-      JOptionPane.showMessageDialog(null, "Could not find server at " + host
-          + " on port " + ChatServer.PORT_NUMBER);
-      System.exit(0);
-    }
-    
-    String message;
-    try {
-      while (true) {
-        message = (String) inputFromServer.readObject();
-        inputFromServerTextArea.append(message + "\n");
-      }
-    } catch (Exception ex) {
-      System.out.println("Client lost server");
-    }
-  }
-
-  public class InputFieldListener implements ActionListener {
-    // Precondition: This client has successfully connected to 
-    // a server and writer is a reference to the server's output stream.
-    public void actionPerformed(ActionEvent ev) {
-
-      try {
-        if (firstEntry) {
-          name = outgoing.getText();
-          firstEntry = false;
-          writer.writeObject(name + " has joined the chat");
-        } else
-          writer.writeObject(name + ": " + outgoing.getText());
-        writer.flush();
-      } catch (Exception ex) {
-      }
-      outgoing.setText("");
-      outgoing.requestFocus();
-    }
-  }
+	public class InputFieldListener implements ActionListener {
+		public void actionPerformed(ActionEvent ev) {
+			client.InputActionPerformed(ev);
+			
+		}
+	}
 }
--- Client.java
+++ Client.java
@@ -1,15 +1,102 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 
 public class Client {
 
 	private ChatClientGUI chatView;
 	private ChatServer server;
-	
+	private String username;
+	private String IP;
+	private String name;
+	private boolean firstEntry = true;
+	private JTextField outgoing;
+	private ObjectOutputStream writer;
+	private ObjectInputStream inputFromServer;
+	private Socket socketServer;
+	public static String host = "localhost";
+
 	public Client() {
-		chatView = new ChatClientGUI();
+		chatView = new ChatClientGUI(this);
 		server = new ChatServer();
+		connectToSuperServer();
 	}
-	
-	public String getUsername() {
-		return chatView.getUsername();
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public static void main() {
+		new Client();
+	}
+
+	public void connectToSuperServer() {
+		try {
+			// host could be "localhost", port could be 4000
+			socketServer = new Socket(host, ChatServer.PORT_NUMBER);
+			writer = new ObjectOutputStream(socketServer.getOutputStream());
+			inputFromServer = new ObjectInputStream(socketServer.getInputStream());
+			System.out.println("Found server who accepted me");
+		} catch (IOException ex) {
+			JOptionPane.showMessageDialog(null,
+					"Could not find server at " + host + " on port " + ChatServer.PORT_NUMBER);
+			System.exit(0);
+		}
+
+		String message;
+		try {
+			while (true) {
+				message = (String) inputFromServer.readObject();
+				chatView.inputFromServerTextArea.append(message + "\n");
+			}
+		} catch (Exception ex) {
+			System.out.println("Client lost server");
+		}
+	}
+
+	public class InputFieldListener implements ActionListener {
+		// Precondition: This client has successfully connected to
+		// a server and writer is a reference to the server's output stream.
+		public void actionPerformed(ActionEvent ev) {
+
+			try {
+				if (firstEntry) {
+					name = outgoing.getText();
+					firstEntry = false;
+					writer.writeObject(name + " has joined the chat");
+				} else
+					writer.writeObject(name + ": " + outgoing.getText());
+				writer.flush();
+			} catch (Exception ex) {
+			}
+			outgoing.setText("");
+			outgoing.requestFocus();
+		}
+	}
+
+	public void InputActionPerformed(ActionEvent ev) {
+		try {
+			if (firstEntry) {
+				name = outgoing.getText();
+				firstEntry = false;
+				writer.writeObject(name + " has joined the chat");
+			} else
+				writer.writeObject(name + ": " + outgoing.getText());
+			writer.flush();
+		} catch (Exception ex) {
+		}
+		outgoing.setText("");
+		outgoing.requestFocus();
+		
 	}
 }
